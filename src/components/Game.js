7df09846d9095cab51a45ef6d80821f2bfe8eefa
Talwind1 <talwind1@gmail.com>
2021-12-17 02:32:07 +0200

    last up for today

--- src/components/Game.js
+++ src/components/Game.js
@@ -28,11 +28,55 @@ class Game extends React.Component {
         players[0].currentScore + this.state.dices[0] + this.state.dices[1];
     } else {
       players[1].currentScore =
-        players[0].currentScore + this.state.dices[0] + this.state.dices[1];
+        players[1].currentScore + this.state.dices[0] + this.state.dices[1];
     }
     this.setState({ players: players });
-    console.log(this.state.players);
   };
+
+  hold = () => {
+    this.updateTotal();
+    this.cleanCurrent();
+    this.swichPlayers();
+  };
+
+  updateTotal = () => {
+    const players = this.state.players;
+    if (this.state.oneIsPlaying) {
+      let value = this.state.players[0].currentScore;
+      players[0].totalScore += value;
+    } else {
+      let value = this.state.players[1].currentScore;
+      players[1].totalScore += value;
+    }
+    this.setState({ players: players });
+  };
+
+  cleanCurrent = () => {
+    const players = this.state.players;
+    if (this.state.oneIsPlaying) {
+      players[0].currentScore = 0;
+    } else {
+      players[1].currentScore = 0;
+    }
+    this.setState({ players: players });
+  };
+
+  swichPlayers = () => {
+    this.setState({ oneIsPlaying: !this.state.oneIsPlaying });
+  };
+
+  initializeState() {
+    this.setState({
+      dices: [null, null],
+      points: 20,
+      players: [
+        { currentScore: 0, totalScore: 0 },
+        { currentScore: 0, totalScore: 0 },
+      ],
+      oneIsPlaying: true,
+      winner: false,
+    });
+  }
 
   render() {
     return (
@@ -43,9 +87,11 @@ class Game extends React.Component {
           total={this.state.players[0].totalScore}
         />
         <div className="buttons">
-          <Dice onClick={this.rollDice} value={this.state.dices}>
-            Roll Dice
-          </Dice>
+          <Dice onClick={this.rollDice}>Roll Dice</Dice>
+          {/* 
+          value={this.state.dices} */}
+          <button onClick={this.initializeState}>New Game</button>
+          <button onClick={this.hold}>hold</button>
         </div>
 
         <Player
